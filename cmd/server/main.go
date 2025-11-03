661156c83ee8efe7e94db74c98ee77af875bdca5
Shehu Muhammad <shehuaamuhammad@gmail.com>
2025-11-03 04:04:12 -0500

    updated the server to publish pause/resume messages to an exchange on a specific routing key

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -6,6 +6,8 @@ import (
 	"os"
 	"os/signal"
 
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/pubsub"
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/routing"
 	amqp "github.com/rabbitmq/amqp091-go"
 )
 
@@ -17,6 +19,17 @@ func main() {
 	}
 	defer conn.Close()
 	fmt.Println("Peril game server connected to RabbitMQ!")
+
+	ch, err := conn.Channel()
+	if err != nil {
+		log.Fatalf("could not open channel: %v", err)
+	}
+	defer ch.Close()
+
+	err = pubsub.PublishJSON(ch, routing.ExchangePerilDirect, routing.PauseKey, routing.PlayingState{IsPaused: true})
+	if err != nil {
+		log.Fatalf("could not send message to exchange: %v", err)
+	}
 
 	// wait for ctrl+c
 	signalChan := make(chan os.Signal, 1)
